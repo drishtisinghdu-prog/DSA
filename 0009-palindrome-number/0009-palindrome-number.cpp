08dfcce4dd92f46de659a31a2457ef83b1ab1df5
Drishti Singh <drishtisingh.du@gmail.com>
2025-11-08 19:12:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (41.73%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int original = x;
+        long rev = 0;
+        while (x != 0) {
+            int digit = x%10;
+            rev = rev*10 + digit;
+            x /= 10;
+        }
+
+        if (int(rev) == original) return true;
+        else return false;
+    }
+};
