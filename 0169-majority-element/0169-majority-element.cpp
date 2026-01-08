0fb9c7a67143c76f212218bf17cc7005533c1f68
Drishti Singh <drishtisingh.du@gmail.com>
2026-01-09 02:05:21 +0530

    Time: 3 ms (28.54%), Space: 28.1 MB (86.54%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map <int,int> hash;
+        int n = nums.size();
+        hash[n] = {0};
+        for(int i=0; i<n; i++) {
+            hash [nums[i]]++;
+        }
+        for(int i=0; i<n; i++) {
+            if(hash[nums[i]] > n/2) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
