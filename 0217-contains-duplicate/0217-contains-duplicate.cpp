a19c7343d7f52cdf01abce11d80c75b3003baece
Drishti Singh <drishtisingh.du@gmail.com>
2026-01-10 00:03:05 +0530

    Time: 26 ms (81.31%), Space: 71.1 MB (79.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int,int> hash;
         int n = nums.size();
-        int maxi = nums[0];
-        for (int i=0; i<n; i++) {
-            maxi = max(maxi,nums[i]);
-        } 
-        hash[maxi] ={0};
-        for(int i=0; i<n; i++) {
-            hash[nums[i]]++;
-        }
-        for(int i=0; i<n; i++) {
-            if(hash[nums[i]] > 1) return true;
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<n; i++) {
+            if(nums[i] == nums[i-1]) return true;
         }
         return false;
     }
